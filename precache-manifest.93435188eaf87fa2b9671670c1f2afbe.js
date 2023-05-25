0469df47e3f90612f08b07ec45ef13a3d618177b
RJT <redbas3@gmail.com>
2023-05-25 15:50:50 +0900

    Updates

---
+++ precache-manifest.93435188eaf87fa2b9671670c1f2afbe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33745626c46566eb9e10",
+    "url": "/nomad_movie/static/js/main.33745626.chunk.js"
+  },
+  {
+    "revision": "f1910fc0215ccc27fd06",
+    "url": "/nomad_movie/static/js/1.f1910fc0.chunk.js"
+  },
+  {
+    "revision": "fea72367bdd97ce17f2e",
+    "url": "/nomad_movie/static/js/runtime~main.fea72367.js"
+  },
+  {
+    "revision": "ecc88f4ea31aa80828f8aa4f3999b867",
+    "url": "/nomad_movie/index.html"
+  }
+];
